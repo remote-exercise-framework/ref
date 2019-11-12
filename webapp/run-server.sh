d7935f2e1bbc740b3821d0cba903e0de05c10b5f
Nils Bars <nils.bars@rub.de>
2019-11-13 00:16:24 +0100

    Final fix for batch update/stop

--- webapp/run-server.sh
+++ webapp/run-server.sh
@@ -5,6 +5,10 @@ set -e
 source upgrade_db.sh
 
 if [[ -z "$DEBUG" || "$DEBUG" == "0" ]]; then
+    #Trust secure headers even if the proxy is not running on localhost.
+    #Without this, X-FORWARDED-PROTO is not trusted and redirects are rewritten
+    #to http.
+    export FORWARDED_ALLOW_IPS="*"
     gunicorn -w 4 -b :8000 'ref:create_app()' --log-level debug --reload
 else
     export FLASK_APP=ref
