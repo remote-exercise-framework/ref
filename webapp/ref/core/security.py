7482ad74044111e10627eaeef5572aac90f99734
Nils Bars <nils.bars@rub.de>
2019-11-13 15:35:30 +0100

    Add path sanitize function

--- webapp/ref/core/security.py
+++ webapp/ref/core/security.py
@@ -2,6 +2,7 @@ from flask_login import login_required, current_user
 from flask import current_app
 from functools import wraps
 from ref.core import flash
+from pathlib import Path
 
 def admin_required(func):
     @wraps(func)
@@ -12,3 +13,15 @@ def admin_required(func):
             return current_app.login_manager.unauthorized()
         return func(*args, **kwargs)
     return login_required(decorated_view)
+
+
+def sanitize_path_is_subdir(parent_path, child_path):
+    if isinstance(parent_path, str):
+        parent_path = Path(parent_path)
+    if isinstance(child_path, str):
+        child_path = Path(child_path)
+
+    parent_path = parent_path.resolve()
+    child_path = child_path.resolve()
+
+    return child_path.as_posix().startswith(parent_path.as_posix())
