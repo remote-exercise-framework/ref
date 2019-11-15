f7a39b2d475d96b64b5c8e46c7795ca9e787f1ba
Nils Bars <nils.bars@rub.de>
2019-11-15 12:39:19 +0100

    Refactor api_provision

--- webapp/ref/view/api.py
+++ webapp/ref/view/api.py
@@ -14,14 +14,14 @@ from flask import (Blueprint, Flask, current_app, jsonify, make_response,
                    redirect, render_template, request, url_for)
 from wtforms import Form, IntegerField, SubmitField, validators
 
+from werkzeug.local import LocalProxy, Local
+
 from ref import db, refbp
 from ref.core import flash, ExerciseImageManager, ExerciseInstanceManager, ExerciseManager
 from ref.model import ConfigParsingError, Exercise, Instance, User
 from ref.model.enums import ExerciseBuildStatus
 
-lerr = lambda msg: current_app.logger.error(msg)
-linfo = lambda msg: current_app.logger.info(msg)
-lwarn = lambda msg: current_app.logger.warning(msg)
+log = LocalProxy(lambda: current_app.logger)
 
 def error_response(msg, code=400):
     msg = jsonify({'error': msg})
@@ -34,14 +34,14 @@ def ok_response(msg):
 def start_and_return_instance(instance: Instance):
     #Check if the instances exercise image is build
     if not ExerciseImageManager(instance.exercise).is_build():
-        lerr(f'User {instance.user} has an instance ({instance}) of an exercise that is not build. Possibly someone delete the docker image?')
+        log.error(f'User {instance.user} has an instance ({instance}) of an exercise that is not build. Possibly someone delete the docker image?')
         return error_response('Inconsistent build state, please notify the system administrator')
 
     instance_manager = ExerciseInstanceManager(instance)
     #Check if the instance is running
     running = instance_manager.is_running()
     if not running:
-        linfo('Instance is not running, starting...')
+        log.info('Instance is not running, starting...')
         instance_manager.stop()
         instance_manager.start()
 
@@ -57,91 +57,95 @@ def start_and_return_instance(instance: Instance):
 @refbp.route('/api/provision', methods=('GET', 'POST'))
 def api_provision():
     """
-    This API endpoint is used by the local ssh-server the students connect to.
-    The request should contain the public-key used for authentication and the
+    Request a instance of a specific exercise for a certain user.
+    This endpoint is called by the SSH entry server and is used to
+    decide how an incoming connection should be redirected, .i.e.,
+    getting the IP address of the container the belongs to the requesting
+    user.
     """
 
     if request.method == 'POST':
-        #content = request.json
-        #Params: username, public key
         content = request.get_json(force=True, silent=True)
         if not content:
+            log.warning('Got provision request without JSON body')
             return error_response('Request is missing JSON body')
 
+        #These are the arguments send by the SSH entry server
+
+        #The user name used for authentication
         exercise_name = content['username']
+
+        #The public key the user used to authenticate
         pubkey = content['pubkey']
 
-        linfo(f'exercise_name={exercise_name}')
-        linfo(f'pubkey={pubkey[:32]}')
+        log.info(f'Request for exercise {exercise_name} for user {pubkey:32} was requested')
 
         user: User = db.get(User, pub_key_ssh=pubkey)
         if not user:
-            lerr('Unknown user!')
+            log.warning('Unable to find user with provided publickey')
             return error_response('Unknown public-key')
 
-        linfo(f'User found {user}')
+        log.info(f'Found matching user: {user}')
 
+        #If we are in maintenance, reject connections from normal users.
         if current_app.config['MAINTENANCE_ENABLED'] and not user.is_admin:
-            linfo('Rejecting connection, since maintenance mode is enabled')
+            log.info('Rejecting connection since maintenance mode is enabled and user is no admin')
             return error_response('-------------------\nSorry, maintenance mode is enabled.\nPlease try again later.\n-------------------')
 
-
         if len(Exercise.query.filter(Exercise.short_name == exercise_name).all()) == 0:
+            log.info('Failed to find exercise with requested name')
             return error_response('No such task')
 
         #Get the default exercise for the requested exercise name
-        default_exercise = Exercise.query.filter(Exercise.short_name == exercise_name).filter(Exercise.is_default == True).first()
-        linfo(f'Default exercise for {exercise_name} is {default_exercise}')
+        default_exercise = Exercise.get_default_exercise(exercise_name)
+        log.info(f'Default exercise for {exercise_name} is {default_exercise}')
 
-        #Get the user instance of requested exercise name (if any)
-        user_instances = user.exercise_instances
-        user_instances = [i for i in user_instances if i.exercise.short_name == exercise_name]
+        #Consistency check
+        #Get the user instance of requested exercise name (if any). This might have a different version then the
+        #default.
+        user_instances = list(filter(lambda instance: instance.exercise.short_name == exercise_name, user.exercise_instances))
         if len(user_instances) > 1:
-            lerr(f'User {user} has more then one instance of the same exercise')
+            log.error(f'User {user} has more then one instance of the same exercise')
             return error_response('Internal error, please notify the system administrator')
 
         user_instance = None
         if len(user_instances):
             user_instance = user_instances[0]
 
-        #If the user has an instance and there is no default for the requested exercise or
-        #the instance is an "instance" of the default exercise, return it.
+        """
+        If the user has an instance of the default verison of the exercise or one that is more recent
+        (i.e., has an high version number than the default) we return it. In case the instance has
+        a lower version than the default, we fall through, since it needs to be updated. Furthermore,
+        if the user has no instance, we also continue to create one.
+        """
         if user_instance and (not default_exercise or user_instance.exercise == default_exercise or user_instance.exercise.version > default_exercise.version):
-            linfo(f'User has an instance of the requested exercise ({user_instance})')
+            log.info(f'User has an instance of the requested exercise: {user_instance}')
             return start_and_return_instance(user_instance)
 
-        #If we are here, the user has no instance of the requested exercise.
+        '''
+        If we are here, one of the following statements is true:
+            1. The user has no instance of the requested exercise.
+            2. The user has an instance, but is is older then the current default version.
+        '''
+
         if not default_exercise:
             return error_response('There is no active default for the requested exercise')
 
-        #The exercise that is the current default.
-        #If an exercise is marked as default, it is guaranteed that there are no instances
-        #of a more recent version of the exercise. Hence, if the user has another instance
-        #of the requested exercise, it must be older.
-
         if not ExerciseImageManager(default_exercise).is_build():
-            lerr(f'Exercise {default_exercise} is marked as default, but is not build! Possibly someone deleted the docker image?')
-            return error_response('Internal error, please notify the system administrator')
-
-        #Now we need to check if the user has an older version of the requested exercise that we need
-        #to update.
-        user_instances = user.exercise_instances
-        user_instances = [i for i in user_instances if i.exercise.short_name == default_exercise.short_name]
-        if len(user_instances) > 1:
-            lerr(f'User {user} has more than 1 instance of exercise {default_exercise.short_name}')
+            log.error(f'Exercise {default_exercise} is marked as default, but is not build! Possibly someone deleted the docker image?')
             return error_response('Internal error, please notify the system administrator')
 
         new_instance = None
-        if len(user_instances):
-            #The user has an older version of the exercise, upgrade it!
-            old_instance = user_instances[0]
-            linfo(f'Found an upgradeable instance ({old_instance})')
+        if user_instance:
+            #The user has an older version of the exercise, upgrade it.
+            old_instance = user_instance
+            log.info(f'Found an upgradeable instance. Upgrading {old_instance} to new version {default_exercise}')
             mgr = ExerciseInstanceManager(old_instance)
             new_instance = mgr.update_instance(default_exercise)
             db.session.commit()
         else:
-            #The user has no older version of the exercise, create a new one
-            linfo(f'User has no instance of exercise {default_exercise}, creating one...')
+            #The user has no instance of the exercise, create a new one.
+            log.info(f'User has no instance of exercise {default_exercise}, creating one...')
             new_instance = ExerciseInstanceManager.create_instance(user, default_exercise)
             db.session.add(new_instance)
             db.session.commit()
@@ -164,7 +168,7 @@ def api_getkeys():
         resp = {
             'keys': keys
         }
-        linfo(f'Returning {len(keys)} public-keys in total.')
+        log.info(f'Returning {len(keys)} public-keys in total.')
         return ok_response(resp)
 
     return error_response('POST expected')
@@ -177,7 +181,7 @@ def api_getuserinfo():
     if request.method == 'POST':
         content = request.json
         pubkey = content['pubkey']
-        linfo(f'pubkey={pubkey[:32]}')
+        log.info(f'pubkey={pubkey[:32]}')
         student = db.get(User, pub_key_ssh=pubkey)
         if student:
             resp = {
