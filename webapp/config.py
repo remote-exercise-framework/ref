fadbd98a68ab5cb482a7ad1b40e96cb98d4cfb49
Nils Bars <nils.bars@rub.de>
2019-11-27 10:52:10 +0100

    Add SECRET_KEY config option

--- webapp/config.py
+++ webapp/config.py
@@ -27,7 +27,7 @@ class ReleaseConfig(object):
     #or the parent directory of the docker-compose.yml file is renamed.
     SSHSERVER_CONTAINER_NAME = 'remote-exercises-framework_sshserver_1'
 
-    SECRET_KEY = os.environ.get('SECRET_KEY') or raise
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SSH_TO_WEB_KEY = os.environ.get('SSH_TO_WEB_KEY')
 
 
