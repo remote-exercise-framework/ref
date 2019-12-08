6e449daff4277c1a8e44f465aa56a30e1dbaf1bd
Nils Bars <nils.bars@rub.de>
2019-12-08 11:51:13 +0100

    Add redis DB and migrate from gunicorn to uwsgi

--- webapp/run-server.sh
+++ webapp/run-server.sh
@@ -7,7 +7,8 @@ if [[ -z "$DEBUG" || "$DEBUG" == "0" ]]; then
     #Without this, X-FORWARDED-PROTO is not trusted and redirects are rewritten
     #to http.
     export FORWARDED_ALLOW_IPS="*"
-    gunicorn -w 4 -b :8000 'ref:create_app()' --log-level debug --reload
+    #gunicorn -w 4 -b :8000 'ref:create_app()' --log-level debug --reload
+    uwsgi --http :8000 --master --processes 4 --manage-script-name --mount "/=ref:create_app()"
 else
     export FLASK_APP=ref
     export FLASK_DEBUG=1
