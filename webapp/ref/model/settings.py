b5fbc4056afe92fa3001ecaf4d3b9ba09b6c0ea4
Nils Bars <nils.bars@rub.de>
2020-01-13 15:48:35 +0100

    Add settings for groups and ssh entry server

--- webapp/ref/model/settings.py
+++ webapp/ref/model/settings.py
@@ -6,17 +6,84 @@ from ref import db
 from flask_login import UserMixin
 from .util import CommonDbOpsMixin, ModelToStringMixin
 from sqlalchemy.orm import backref
+from enum import Enum
+from flask import current_app
+
+default_ssh_welcome_msg = """
+  ____  ____  ____                 _ __
+ / __ \/ __/ / __/__ ______ ______(_) /___ __
+/ /_/ /\ \  _\ \/ -_) __/ // / __/ / __/ // /
+\____/___/ /___/\__/\__/\_,_/_/ /_/\__/\_, /
+                                      /___/"""
+
+class SystemSettingKeys:
+    GROUPS_ENABLED = 'GROUPS_ENABLED'
+    GROUP_SIZE = 'GROUP_SIZE'
+    SSH_WELCOME_MSG = 'SSH_WELCOME_MSG'
 
 class SystemSetting(CommonDbOpsMixin, ModelToStringMixin, db.Model):
+
     __to_str_fields__ = ['id', 'name']
     __tablename__ = 'system_setting'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text(), nullable=False, unique=True)
-    value = db.Column(PickleType(), nullable=True)
+    value = db.Column(db.PickleType(), nullable=True)
 
     @staticmethod
-    def user_groups_enabled() -> bool:
-        pass
+    def get_setting(name: SystemSettingKeys):
+        res = SystemSetting.query.filter(SystemSetting.name == name).one_or_none()
+        return res
 
-    def user_groups_size_limit() -> int:
-        pass
+    @staticmethod
+    def get_user_groups_enabled() -> bool:
+        is_enabled = SystemSetting.get_setting(SystemSettingKeys.GROUPS_ENABLED)
+        return is_enabled and is_enabled.value is True
+
+    @staticmethod
+    def set_user_groups_enabled(enabled: bool):
+        assert isinstance(enabled, bool)
+        is_enabled = SystemSetting.get_setting(SystemSettingKeys.GROUPS_ENABLED)
+        if is_enabled is None:
+            is_enabled = SystemSetting()
+            is_enabled.name = SystemSettingKeys.GROUPS_ENABLED
+        is_enabled.value = enabled
+        current_app.db.session.add(is_enabled)
+        current_app.db.session.commit()
+
+    @staticmethod
+    def get_user_groups_size_limit() -> int:
+        group_size = SystemSetting.get_setting(SystemSettingKeys.GROUP_SIZE)
+        if group_size is None:
+            return None
+        else:
+            return group_size.value
+
+    @staticmethod
+    def set_user_groups_size_limit(max_size: int):
+        assert isinstance(max_size, int)
+        group_size = SystemSetting.get_setting(SystemSettingKeys.GROUP_SIZE)
+        if group_size is None:
+            group_size = SystemSetting()
+            group_size.name = SystemSettingKeys.GROUP_SIZE
+        group_size.value = max_size
+        current_app.db.session.add(group_size)
+        current_app.db.session.commit()
+
+    @staticmethod
+    def get_ssh_welcome_header() -> str:
+        msg = SystemSetting.get_setting(SystemSettingKeys.SSH_WELCOME_MSG)
+        if msg is None:
+            return default_ssh_welcome_msg
+        else:
+            return msg.value
+
+    @staticmethod
+    def set_ssh_welcome_header(msg: str):
+        assert isinstance(msg, str)
+        setting_msg = SystemSetting.get_setting(SystemSettingKeys.SSH_WELCOME_MSG)
+        if setting_msg is None:
+            setting_msg = SystemSetting()
+            setting_msg.name = SystemSettingKeys.SSH_WELCOME_MSG
+        setting_msg.value = msg
+        current_app.db.session.add(setting_msg)
+        current_app.db.session.commit()
--- webapp/ref/view/system_settings.py
+++ webapp/ref/view/system_settings.py
@@ -5,19 +5,46 @@ from flask import (Blueprint, Flask, Response, current_app, redirect,
                    render_template, request, url_for)
 from itsdangerous import URLSafeTimedSerializer
 
+from werkzeug.local import LocalProxy
 from Crypto.PublicKey import RSA
 from ref import db, refbp
 from ref.core import admin_required, flash
 from ref.core.util import redirect_to_next
-from ref.model import User, UserGroup
+from ref.model import User, UserGroup, SystemSetting
 from ref.model.enums import CourseOfStudies
 from wtforms import (BooleanField, Form, IntegerField, PasswordField,
                      RadioField, StringField, SubmitField, TextField,
                      validators, SelectField)
 
+log = LocalProxy(lambda: current_app.logger)
+
+class GroupSettings(Form):
+    group_size = IntegerField('Max. Group Size')
+    groups_enable = BooleanField('Groups Enabled')
+    submit = SubmitField('Save')
+
+class SshSettings(Form):
+    welcome_header = TextField('SSH Welcome Header')
+    submit = SubmitField('Save')
 
 @refbp.route('/admin/system/settings/', methods=('GET', 'POST'))
 @admin_required
 def view_system_settings():
-    return render_template('system_settings.html')
+
+    #Group settings belong here
+    group_settings = GroupSettings(request.form, prefix='group_settings')
+    if group_settings.submit.data and group_settings.validate():
+        SystemSetting.set_user_groups_size_limit(group_settings.group_size.data)
+        SystemSetting.set_user_groups_enabled(group_settings.groups_enable.data)
+    else:
+        group_settings.group_size.data = SystemSetting.get_user_groups_size_limit()
+        group_settings.groups_enable.data = SystemSetting.get_user_groups_enabled()
+
+    ssh_settings = SshSettings(request.form, prefix='ssh_settings')
+    if ssh_settings.submit.data and ssh_settings.validate():
+        SystemSetting.set_ssh_welcome_header(ssh_settings.welcome_header.data)
+    else:
+        ssh_settings.welcome_header.data = SystemSetting.get_ssh_welcome_header()
+
+    return render_template('system_settings.html', group_settings=group_settings, ssh_settings=ssh_settings)
 
