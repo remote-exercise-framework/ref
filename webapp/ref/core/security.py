be5fbd465f254de3dbbc20458285d650fbf6f74a
Nils Bars <nils.bars@rub.de>
2020-03-10 11:37:02 +0100

    Add grading assistant auth test function wrapper

--- webapp/ref/core/security.py
+++ webapp/ref/core/security.py
@@ -4,19 +4,43 @@ from functools import wraps
 from ref.core import flash
 from pathlib import Path
 from werkzeug.local import LocalProxy
+from ref.model.enums import UserAuthorizationGroups
 
 log = LocalProxy(lambda: current_app.logger)
 
 def admin_required(func):
+    """
+    Only allow admins to access the given view.
+    """
     @wraps(func)
     def decorated_view(*args, **kwargs):
-        if 'LOGIN_DISABLED' in current_app.config and current_app.config['LOGIN_DISABLED']:
-            pass
-        elif not current_user.is_admin:
+        if UserAuthorizationGroups.ADMIN not in current_user.auth_groups:
             return current_app.login_manager.unauthorized()
         return func(*args, **kwargs)
     return login_required(decorated_view)
 
+def grading_assistant_required(func):
+    """
+    Only allow admins and grading assistants to access the given view.
+    """
+    @wraps(func)
+    def decorated_view(*args, **kwargs):
+        if UserAuthorizationGroups.GRADING_ASSISTANT not in current_user.auth_groups and UserAuthorizationGroups.ADMIN not in current_user.auth_groups:
+            return current_app.login_manager.unauthorized()
+        return func(*args, **kwargs)
+    return login_required(decorated_view)
+
+def group_required(func, *groups):
+    @wraps(func)
+    def decorated_view(*args, **kwargs):
+        user_auth_groups = current_user.auth_groups
+        for g in groups:
+            assert isinstance(g, UserAuthorizationGroups)
+            if g not in user_auth_groups:
+                return current_app.login_manager.unauthorized()
+        return func(*args, **kwargs)
+
+    return login_required(decorated_view)
 
 def sanitize_path_is_subdir(parent_path, child_path):
     if isinstance(parent_path, str):
