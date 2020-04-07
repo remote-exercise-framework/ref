5c318644a45a9d6165dee7ca0ec57ef6a04d1962
Nils Bars <nils.bars@rub.de>
2020-04-07 15:42:18 +0200

    Update task.py script

--- ref-docker-base/task.py
+++ ref-docker-base/task.py
@@ -3,14 +3,25 @@
 import argparse
 import os
 import socket
+import subprocess
 import sys
 import time
 import traceback
 
 import requests
+from colorama import Back, Fore, Style
 from itsdangerous import TimedSerializer
 
 sys.path.append('/usr/local/lib/python3.7/site-packages')
+
+def print_ok(*args, **kwargs):
+    print(Fore.GREEN, *args, Style.RESET_ALL, **kwargs, sep='')
+
+def print_warn(*args, **kwargs):
+    print(Fore.YELLOW, *args, Style.RESET_ALL, **kwargs, sep='')
+
+def print_err(*args, **kwargs):
+    print(Fore.RED, *args, Style.RESET_ALL, **kwargs, sep='')
 
 with open('/etc/key', 'rb') as f:
     KEY = f.read()
@@ -29,41 +40,43 @@ def handle_response(resp):
         err = resp.status_code
         json = resp.json()
     except ValueError:
-        print('Missing JSON body')
+        print_err('[!] Missing JSON body')
         return False
     except Exception:
-        print('Internal Error')
+        print_err('[!] Internal Error')
         return False
     else:
         if err != 200:
             if 'error' in json:
-                print(json['error'])
+                print_err(f'[!]', json['error'])
             else:
-                print('Unknown error, please contact the staff.')
+                print_err('[!]', 'Unknown error, please contact the staff.')
             return False
         else:
-            print(json)
+            print_ok('[+] ', json)
     return True
 
 def check_answer(prompt=None):
-    data = input(prompt)
+    if prompt:
+        print_warn(prompt, end='')
+    data = input()
     data = data.lower()
     return data == 'y' or data == 'yes' or data == 'true'
 
 
 def cmd_reset(args):
-    print('This operation will delete all data of this instance!')
+    print_ok('[+] This operation will delete all data of this instance!')
     if not check_answer('Continue [y/n]'):
         exit(0)
 
-    print('Resetting instance...', flush=True)
+    print_ok('[+] Resetting instance...', flush=True)
     req = {}
     req = finalize_request(req)
     res = requests.post('http://sshserver:8000/api/instance/reset', json=req)
     handle_response(res)
 
 def cmd_submit(args):
-    print('Submitting instance...', flush=True)
+    print_ok('[+] Submitting instance...', flush=True)
 
     req = {}
     req = finalize_request(req)
@@ -74,7 +87,21 @@ def cmd_presubmit(args):
     """
     Run a script that is specific to the current task and print its output?
     """
-    pass
+    """
+    1. Execute some script that is exercise specific and returns
+        - Enivron test passed (e.g., correct file name) test "environ"
+        - Exercise test passed (user provided script successfully spawns a shell) test "result"
+    """
+    test_path = '/usr/local/bin/submission-tests'
+    if not os.path.isfile(test_path):
+        print_warn('[+] No testsuit found, skipping...')
+        exit(2)
+
+    ret = subprocess.run(test_path, shell=False, check=False)
+    if ret.returncode == 0:
+        exit(0)
+    else:
+        exit(1)
 
 def main():
     parser = argparse.ArgumentParser()
@@ -98,7 +125,6 @@ def main():
 
 
     args = parser.parse_args()
-    print(args)
     args.func(args)
 
 if __name__ == "__main__":
