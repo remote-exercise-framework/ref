4bbdb0c7ace0049f821f552e346bad57629677bc
Nils Bars <nils.bars@rub.de>
2020-04-26 14:21:49 +0200

    Refactor settings view

--- webapp/ref/view/system_settings.py
+++ webapp/ref/view/system_settings.py
@@ -14,39 +14,54 @@ import pytz
 from ref import db, refbp
 from ref.core import admin_required, flash
 from ref.core.util import redirect_to_next
-from ref.model import SystemSettingsManager, User, UserGroup
-from ref.model.enums import CourseOfStudies
+from ref.model import SystemSettingsManager, UserGroup
 
 log = LocalProxy(lambda: current_app.logger)
 
-def field_to_str(form, field):
+
+def field_to_str(_, field):
     return str(field.data)
+
 
 class GeneralSettings(Form):
     submit = SubmitField('Save')
     course_name = TextField('Course Name')
-    allow_submission_deletion = BooleanField('Allow admins to delete submissions')
-    maintenance_enabled = BooleanField('Enable maintenance mode: Disallow any new access by non admin users. Already established connections are not closed')
+    allow_submission_deletion = BooleanField(
+        'Allow admins to delete submissions')
+    maintenance_enabled = BooleanField(
+        'Enable maintenance mode: Disallow any new access by non admin users. Already established connections are not closed'
+        )
     disable_submission = BooleanField('Disable submission for instances.')
-    timezone = SelectField('Timezone that is used for datetime representation in case no timezone information is provided by the client.', choices=[(e, e) for e in pytz.all_timezones])
+    timezone = SelectField(
+        'Timezone that is used for datetime representation in case no timezone information is provided by the client.',
+        choices=[(e, e) for e in pytz.all_timezones]
+        )
+
 
 class GroupSettings(Form):
     group_size = IntegerField('Max. group size')
     groups_enable = BooleanField('Groups enabled')
     submit = SubmitField('Save')
 
+
 class SshSettings(Form):
     ssh_hostname = TextField('SSH Host')
     ssh_port = TextField('SSH port', validators=[])
     welcome_header = TextField('SSH Welcome Header')
-    allow_none_default_provisioning = BooleanField('Allow admins to provision non default container')
-    ssh_instance_introspection = BooleanField('Allow admins and grading assistance to access arbitrary instances using instance-{ID} as username')
+    allow_none_default_provisioning = BooleanField(
+        'Allow admins to provision non default container'
+        )
+    ssh_instance_introspection = BooleanField(
+        'Allow admins and grading assistance to access arbitrary instances using instance-{ID} as username'
+        )
     submit = SubmitField('Save')
+
 
 @refbp.route('/admin/system/settings/', methods=('GET', 'POST'))
 @admin_required
 def view_system_settings():
 
+    # General settings
     general_settings = GeneralSettings(request.form, prefix='general_settings')
     if general_settings.submit.data and general_settings.validate():
         SystemSettingsManager.COURSE_NAME.value = general_settings.course_name.data
@@ -61,7 +76,7 @@ def view_system_settings():
         general_settings.disable_submission.data = SystemSettingsManager.SUBMISSION_DISABLED.value
         general_settings.timezone.data = SystemSettingsManager.TIMEZONE.value
 
-    #Group settings belong here
+    # Group settings belong here
     group_settings = GroupSettings(request.form, prefix='group_settings')
     if group_settings.submit.data and group_settings.validate():
         SystemSettingsManager.GROUP_SIZE.value = group_settings.group_size.data
@@ -70,6 +85,7 @@ def view_system_settings():
         group_settings.group_size.data = SystemSettingsManager.GROUP_SIZE.value
         group_settings.groups_enable.data = SystemSettingsManager.GROUPS_ENABLED.value
 
+    # SSH settings
     ssh_settings = SshSettings(request.form, prefix='ssh_settings')
     if ssh_settings.submit.data and ssh_settings.validate():
         SystemSettingsManager.SSH_HOSTNAME.value = ssh_settings.ssh_hostname.data
@@ -82,6 +98,11 @@ def view_system_settings():
         ssh_settings.ssh_port.data = SystemSettingsManager.SSH_PORT.value
         ssh_settings.welcome_header.data = SystemSettingsManager.SSH_WELCOME_MSG.value
         ssh_settings.ssh_instance_introspection.data = SystemSettingsManager.INSTANCE_SSH_INTROSPECTION.value
-        ssh_settings.allow_none_default_provisioning.data =  SystemSettingsManager.INSTANCE_NON_DEFAULT_PROVISIONING.value
+        ssh_settings.allow_none_default_provisioning.data = SystemSettingsManager.INSTANCE_NON_DEFAULT_PROVISIONING.value
 
-    return render_template('system_settings.html', group_settings=group_settings, ssh_settings=ssh_settings, general_settings=general_settings)
+    return render_template(
+        'system_settings.html',
+        group_settings=group_settings,
+        ssh_settings=ssh_settings,
+        general_settings=general_settings
+        )
