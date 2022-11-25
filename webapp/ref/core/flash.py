cca7ce893562c140f2bbf8939ef02395099c09d4
Nils Bars <nils.bars@rub.de>
2022-11-25 16:22:56 +0100

    Update REF utils

--- webapp/ref/core/flash.py
+++ webapp/ref/core/flash.py
@@ -3,16 +3,12 @@ import flask
 
 def success(msg):
     flask.flash(msg, 'success')
-    flask.current_app.logger.debug(f'flash.success({msg})')
 
 def warning(msg):
     flask.flash(msg, 'warning')
-    flask.current_app.logger.debug(f'flash.warning({msg})')
 
 def info(msg):
     flask.flash(msg, 'info')
-    flask.current_app.logger.debug(f'flash.info({msg})')
 
 def error(msg):
     flask.flash(msg, 'error')
-    flask.current_app.logger.debug(f'flash.error({msg})')
