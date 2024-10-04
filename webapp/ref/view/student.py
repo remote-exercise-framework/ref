ea48f757960d73bdf50e41c0a90b6d823b77f7b1
Florian Bauckholt <florian.bauckholt@cispa.de>
2024-10-04 15:19:05 +0200

    webapp: support ed25519 ssh keys
    The pycryptodome dependency bump is necessary to pull in upstream
    support for curve25519 among others

--- webapp/ref/view/student.py
+++ webapp/ref/view/student.py
@@ -2,7 +2,7 @@ import datetime
 import re
 from functools import partial
 
-from Crypto.PublicKey import RSA
+from Crypto.PublicKey import RSA, ECC
 from flask import (
     Blueprint,
     Flask,
@@ -102,11 +102,13 @@ def validate_pubkey(form, field):
     if field.data is None or field.data == "":
         return
 
-    for fn in [RSA.import_key]:
+    for fn in [RSA.import_key, ECC.import_key]:
         try:
             # Replace the key with the parsed one, thus we use everywhere exactly
             # the same string to represent a specific key.
-            key = fn(field.data).export_key(format="OpenSSH").decode()
+            key = fn(field.data).export_key(format="OpenSSH")
+            if isinstance(key, bytes):
+                key = key.decode()
             field.data = key
             return key
         except:
